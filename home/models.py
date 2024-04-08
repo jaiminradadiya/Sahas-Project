f3a43186ede65c18f411a07cf4acdb013131d57d
Radadiya Jaimin <radadiyajaimin31@gmail.com>
2024-04-08 17:16:23 +0530

    models change

--- home/models.py
+++ home/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-class Product(models.Model):
-    id    = models.AutoField(primary_key=True)
-    name  = models.CharField(max_length = 100) 
-    info  = models.CharField(max_length = 100, default = '')
-    price = models.IntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-from django.db import models
-
-# Create your models here.
 class Gallery(models.Model):
     image_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
